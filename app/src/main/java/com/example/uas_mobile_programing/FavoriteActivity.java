364bf40778fa4a30cc45f9a93ebab6a38ea86b0c
Rickardo130702 <Rikardo@students.amikom.ac.id>
2023-07-26 11:19:47 +0700

    Penambahan Detail Burjo Borneo

--- app/src/main/java/com/example/uas_mobile_programing/FavoriteActivity.java
+++ app/src/main/java/com/example/uas_mobile_programing/FavoriteActivity.java
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,6 +19,16 @@ public class FavoriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favorite);
+
+        RelativeLayout relativeLayout = findViewById(R.id.rlBurjoBorneo2);
+
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FavoriteActivity.this, BurjoBorneoActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
